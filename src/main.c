27d9fe1f0f73081a15650bb38f9a9354b86638b9
w1ntap3 <cavid.guluzadeh@gmail.com>
2025-06-07 02:02:07 +0400

    Enhance removeCategory command to support forced removal of categories and associated expenses

--- src/main.c
+++ src/main.c
@@ -1,5 +1,4 @@
-//removeCategory doesnt work if an expense uses that category. removecategory force removes the category AND expenses that stored that category.
-//changing only the values you want while editExpense
+//removeCategory doesnt work if an expense uses that category. removecategory FORCED removes the category AND expenses that stored that category.
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -358,43 +357,132 @@ int main(int argc, char *argv[])
             sqlite3_finalize(categoryStmt);
         } else if (!strcmp(verb, "removeCategory"))
         {
-            sqlite3_stmt *rmEstmt;
-            const char *rmQuery = "DELETE FROM categories WHERE (id=?);";
-            char inputRmId[MAX_LENGTH];
-            int rmId;
+            // statement for cat removal
+            sqlite3_stmt *rmCstmt;
+            const char *rmQuery = "DELETE FROM categories WHERE id=?;";
 
-            printf("ID of the category you would like to remove >> ");
-            if(!fgets(inputRmId, MAX_LENGTH, stdin)) continue;
-            inputRmId[strcspn(inputRmId, "\n")] = '\0';
-            rmId = atoi(inputRmId);
+            // input args
+            char inputCatName[MAX_LENGTH];
+            char forced[MAX_LENGTH];
 
-            if (sqlite3_prepare_v2(db, rmQuery, -1, &rmEstmt, NULL) != SQLITE_OK)
+            // Category ID found through name
+            int foundId = -1;
+
+            sscanf(input, "removeCategory %s %s", inputCatName, forced);            
+            // statement for cat id via cat name
+            sqlite3_stmt *getIdstmt;
+            const char *getIdQuery = "SELECT id from categories WHERE name = ?;";
+            
+            if (sqlite3_prepare_v2(db, getIdQuery, -1, &getIdstmt, 0) != SQLITE_OK)
+            {
+                fprintf(stderr, "Failed to prepare the getId statement. %s\n", sqlite3_errmsg(db));
+                continue;
+            }
+
+            if (sqlite3_bind_int(getIdstmt, 1, inputCatName) != SQLITE_OK) {
+                fprintf(stderr, "Failed to bind the Id to the statement. %s\n", sqlite3_errmsg(db));
+                continue;
+            }
+            
+            int stepResult = sqlite3_step(getIdstmt);
+
+            if (stepResult == SQLITE_ROW) {
+                foundId = sqlite3_column_int(getIdstmt, 0); // successful step --> found an id
+            } else if (stepResult == SQLITE_DONE) {
+                printf("No such category named \"%s\"", inputCatName); // successful step --> category doesnt exist
+                continue;
+            } else {
+                fprintf(stderr, "Failed to properly execute the getId statement. %s\n", sqlite3_errmsg(db)); // failed step (error)
+                continue;
+            }
+
+            sqlite3_finalize(getIdstmt);
+
+            // statement for removing expense records
+            sqlite3_stmt *removeEstmt;
+            const char *removeEquery = "DELETE FROM expenses WHERE category_id = ?;";
+            
+            if (sqlite3_prepare_v2(db, removeEquery, -1, &removeEstmt, 0) != SQLITE_OK) {
+                fprintf(stderr, "Failed to prepare the check statement. %s\n", sqlite3_errmsg(db));
+                continue;
+            }
+
+            if (sqlite3_bind_int(removeEstmt, 1, foundId) != SQLITE_OK) {
+                fprintf(stderr, "Failed to bind ID to the statement. %s\n", sqlite3_errmsg(db));
+                continue;
+            }
+
+            if (sqlite3_prepare_v2(db, rmQuery, -1, &rmCstmt, NULL) != SQLITE_OK)
             {
                 fprintf(stderr, "Failed to prepare a statement. %s\n", sqlite3_errmsg(db));
                 continue;
             }
 
-            if (sqlite3_bind_int(rmEstmt, 1, rmId) != SQLITE_OK)
+            if (sqlite3_bind_int(rmCstmt, 1, foundId) != SQLITE_OK)
             {
                 fprintf(stderr, "Failed to bind ID to the statement. %s\n", sqlite3_errmsg(db));
                 continue;
             }
+            
+            if (!strcmp(forced, "FORCED")) {
+                if (sqlite3_step(removeEstmt) == SQLITE_DONE) {
+                printf("Successfully removed all expense records with \"%s\".\n", inputCatName);
+                }
 
-            if (sqlite3_step(rmEstmt) != SQLITE_DONE)
-            {
-                fprintf(stderr, "Failed to execute the statement. %s\n", sqlite3_errmsg(db));
-                continue;
-            } else
-            {
-                printf("Successfully removed category with ID \"%d\"\n", rmId);
+                if (sqlite3_step(rmCstmt) == SQLITE_DONE)
+                {
+                    printf("Successfully removed category \"%s\"(ID %d)\n", inputCatName, foundId);
+                    return SQLITE_OK;
+                } else
+                {
+                    fprintf(stderr, "Failed to execute the statement. %s\n", sqlite3_errmsg(db));
+                    return SQLITE_ERROR;
+                }
+
+                sqlite3_finalize(removeEstmt);
+                sqlite3_finalize(rmCstmt);
+            } else {
+                sqlite3_finalize(removeEstmt);
+
+                sqlite3_stmt *anyRowsStmt;
+                const char *anyRowsQuery = "SELECT * FROM expenses WHERE category_id = ?;";
+                int anyRows = -1;
+
+                sqlite3_prepare_v2(db,anyRowsQuery,-1,&anyRowsStmt, 0);
+                sqlite3_bind_int(anyRowsStmt, 1, foundId);
+                if (sqlite3_step(anyRowsStmt) == SQLITE_ROW) {
+                    anyRows = 0; // Found rows
+                } else {
+                    anyRows = 1; // No rows
+                }
+                sqlite3_finalize(anyRowsStmt);
+                
+                if (anyRows) {
+                    if (sqlite3_step(rmCstmt) == SQLITE_DONE)
+                    {
+                        printf("Successfully removed category \"%s\"(ID %d)\n", inputCatName, foundId);
+                        return SQLITE_OK;
+                    } else
+                    {
+                        fprintf(stderr, "Failed to execute the statement. %s\n", sqlite3_errmsg(db));
+                        return SQLITE_ERROR;
+                    }
+
+                    sqlite3_finalize(rmCstmt);
+                } else {
+                    printf(
+                        "Cannot remove the category \"%s\": There are expense records with this category.\n"
+                        "(Try \"removeCategory [CATEGORY_NAME] FORCED\" to remove both category and expenses)", inputCatName);
+                }
             }
-
-            sqlite3_finalize(rmEstmt);
         } else if (!strcmp(verb, "help"))
         {
             printf(
                 "| %-60s | %45s |\n"
                 "================================================================================================================\n"
+                "| %-60s | %45s |\n"
+                "| %-60s | %45s |\n"
+                "| %-60s | %45s |\n"
                 "| %-60s | %45s |\n"
                 "| %-60s | %45s |\n"
                 "| %-60s | %45s |\n"
@@ -410,7 +498,10 @@ int main(int argc, char *argv[])
                 "removeExpense [ID]", "Remove an expense record",
                 "editExpense [ID] [AMOUNT] [CATEGORY] [DATE] '[DESCRIPTION]'", "Edit an expense record",
                 "newCategory [NAME]", "Create a new expense category record",
-                "removeCategory [ID]", "Remove an expense category record",
+                "removeCategory [ID]", "Remove a category record (Will not pass ",
+                "", "through if category is used somewhere)",
+                "removeCategory [ID] forced", "Removes a category record + all the expenses",
+                "", "with that category",
                 "website", "Website for visualizing your database",
                 "kill", "Kill sql3-jaja",
                 "help", "List of all commands");
